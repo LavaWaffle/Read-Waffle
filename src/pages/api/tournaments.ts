bee167569d4b3e54571dfd82c5c8e969953127c2
LavaWaffle <bjxobjxo@gmail.com>
2022-08-08 22:18:49 +0000

    updated api

---
+++ src/pages/api/tournaments.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../server/db/client";
+
+const tournaments = async (req: NextApiRequest, res: NextApiResponse) => {
+  res.setHeader('Cache-Control', 'max-age=0, s-maxage=10, stale-while-revalidate=10');
+  const tournaments =  await prisma.game.findMany({
+    select: {
+      tournament: true,
+    }
+  });
+  const tournamentsList = tournaments.map(tournament => tournament.tournament);
+  // return unique tournaments
+  const uniqueTournaments = [...new Set(tournamentsList)];
+  res.status(200).json(uniqueTournaments);
+};
+
+export default tournaments;
