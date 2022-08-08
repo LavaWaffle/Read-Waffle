31bb63dbca4fd9ce0420060c10585cbb5d34f56a
LavaWaffle <bjxobjxo@gmail.com>
2022-08-08 22:26:39 +0000

    experimental edge

--- src/pages/api/games.ts
+++ src/pages/api/games.ts
@@ -2,6 +2,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../server/db/client";
 
+export const config = {
+  runtime: 'experimental-edge'
+}
+
 const game = async (req: NextApiRequest, res: NextApiResponse) => {
   res.setHeader('Cache-Control', 'max-age=0, s-maxage=10, stale-while-revalidate=10');
   const examples = await prisma.game.findMany({
