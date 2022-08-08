d2d2136a8dc6266f5b86ba1287f5d37b441209fe
LavaWaffle <60158215+LavaWaffle@users.noreply.github.com>
2022-08-07 20:44:17 -0400

    added nextjs api cache

--- src/pages/api/game.ts
+++ src/pages/api/game.ts
@@ -3,6 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../server/db/client";
 
 const examples = async (req: NextApiRequest, res: NextApiResponse) => {
+  res.setHeader('Cache-Control', 'max-age=0, s-maxage=10, stale-while-revalidate=10');
   const examples = await prisma.game.findMany({
     orderBy: {
       createdAt: "desc",
