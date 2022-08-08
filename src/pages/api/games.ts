97903f909dc82b5b36e84ec67a1ec2957d2a250d
LavaWaffle <bjxobjxo@gmail.com>
2022-08-08 22:30:09 +0000

    removed edge

--- src/pages/api/games.ts
+++ src/pages/api/games.ts
@@ -2,10 +2,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../server/db/client";
 
-export const config = {
-  runtime: 'experimental-edge'
-}
-
 const game = async (req: NextApiRequest, res: NextApiResponse) => {
   res.setHeader('Cache-Control', 'max-age=0, s-maxage=10, stale-while-revalidate=10');
   const examples = await prisma.game.findMany({
--- src/pages/api/tournaments.ts
+++ src/pages/api/tournaments.ts
@@ -1,10 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../server/db/client";
 
-export const config = {
-  runtime: 'experimental-edge'
-}
-
 const tournaments = async (req: NextApiRequest, res: NextApiResponse) => {
   res.setHeader('Cache-Control', 'max-age=0, s-maxage=10, stale-while-revalidate=10');
   const tournaments =  await prisma.game.findMany({
