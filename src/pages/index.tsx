0482975aedf2aa62dfdd691da905119f5b3e154e
LavaWaffle <60158215+LavaWaffle@users.noreply.github.com>
2022-08-09 12:50:07 -0400

    added base functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,32 @@
 import { useGamesContext } from "@/context/GamesContext";
 import type { NextPage } from "next";
+import ImageMarker from "react-image-marker";
 
 const Home: NextPage = () => {  
   const { currentGame } = useGamesContext();
-  
+  if (currentGame === undefined) return (
+    // game isn't defined
+    // ask user to select a game
+    <div>Select a Game</div>
+  )
+  // grabs markers from current game
+  const markers = currentGame.markers.map(marker => {
+    if (marker.left === null && marker.top === null) return null;
+    return {
+      top: Number(marker.top),
+      left: Number(marker.left),
+    }
+  })
+  // remove null markers (idk how it works either)
+  const filteredMarkers = markers.filter((marker): marker is { top: number, left: number} => {
+    return marker !== null;
+  });
   return (
     <>
-      <h1>{currentGame ? currentGame.name : 'Select a game'}</h1>
+      <ImageMarker 
+        src="field.png"
+        markers={filteredMarkers}
+      />      
     </>
   );
 };
